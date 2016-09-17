bbfd524f3e475c19887fafe401adf48943369a6e
regireed89 <regireed89@gmail.com>
2016-09-17 18:30:16 -0500

    String Class (unfinished)

--- String Class/MyString.cpp
+++ String Class/MyString.cpp
@@ -15,12 +15,12 @@ MyString::MyString(char newString[])
 
 int MyString::Length()
 {
-// function finds the length of character array
-// loops through array until it reaches an exit point then returns how many indexs it went through	
+	// function finds the length of character array
+	// loops through array until it reaches an exit point then returns how many indexs it went through	
 	int i;
-	for ( i = 0; m_string[i] != '\0'; i++)
+	for (i = 0; m_string[i] != '\0'; i++)
 	{
-		
+
 	}
 
 	return i;
@@ -28,30 +28,41 @@ int MyString::Length()
 
 void MyString::Print()
 {
-// find a charcter at a specific index of a character array
-// 
+	// find a charcter at a specific index of a character array
+	// 
 	for (int i = 0; i < Length(); i++)
-	{		
+	{
 		cout << m_string[i] << endl;
 	}
 
-	
+
 }
 
 char MyString::Compare()
 {
-	char Comp[7] =  "turtle";
+	char Comp[7] = "turyle";
 	int i;
 	for (i = 0; i < Length(); i++)
 	{
-		if (Comp[i] == m_string[i])
+		for (i = 0; i != Comp[i] && m_string[i]; i++)
 		{
-			return true;
-		}
-		else
-		{
-			return false;
+			if (Comp[i] == m_string[i])
+			{
+				cout << i << endl;
+			}
+			else
+			{
+				return false;
+			}
 		}
 	}
+	return i;
+}
+
+char MyString::StrAppend()
+{
+
+
+	return 0;
 }
 
