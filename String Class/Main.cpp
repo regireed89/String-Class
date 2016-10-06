cbfd795bafa35b2fd9c084faaef0d8993d203dce
regireed89 <regireed89@gmail.com>
2016-10-06 12:01:31 -0500

    string class

--- String Class/Main.cpp
+++ String Class/Main.cpp
@@ -12,7 +12,7 @@ int main()
 	cout << "my two srtings are turtle and append" << endl;
 	cout <<"the length of the first string is: " << s.Length() << endl;
 	cout << s.Compare(other) << endl;
-	std::cout <<"this is the strings being appended" << s.Append(other).SetCStyle() << std::endl;
+	std::cout <<"this is the strings being appended: " << s.Append(other).SetCStyle() << std::endl;
 	cout<<"returning as a constant char" << s.Return() << endl;
 	cout <<"is the string uppercase" << s.ConvertUp() << endl;
 	cout <<"is the string lowercase" << s.ConvertLow() << endl;
