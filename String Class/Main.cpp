fc5f7ff3b6d34e90be873911327b3dfc5bac4f77
regireed89 <regireed89@gmail.com>
2016-10-06 11:46:13 -0500

    string class

--- String Class/Main.cpp
+++ String Class/Main.cpp
@@ -8,11 +8,12 @@ int main()
 {
 	
 	MyString s = MyString("turtle");
-	MyString other = MyString("poop");
-	cout <<"the length of the string is: " << s.Length() << endl;
+	MyString other = MyString("append");
+	cout << "my two srtings are turtle and append" << endl;
+	cout <<"the length of the first string is: " << s.Length() << endl;
 	cout << s.Compare(other) << endl;
-	std::cout << s.Append(other).SetCStyle() << std::endl;
-	cout<<s.Return() << endl;
+	std::cout <<"this is the strings being appended" << s.Append(other).SetCStyle() << std::endl;
+	cout<<"returning as a constant char" << s.Return() << endl;
 	cout <<"is the string uppercase" << s.ConvertUp() << endl;
 	cout <<"is the string lowercase" << s.ConvertLow() << endl;
 	system("pause");
